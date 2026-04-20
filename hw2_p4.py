ccf5e8f21d4ecfedfaf7597ea1df46d27310f2ce
Benjamin Doo <bendoo529@gmail.com>
2026-04-19 23:42:06 -0700

    plot of raised cosine vs sinc in freq domain

---
+++ hw2_p4.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+start = -4
+stop = 4
+samples = 801
+sps = 100
+B = 0.5 # rolloff factor
+
+t = np.linspace(start, stop, samples)
+f = np.fft.fftfreq(samples, (t[1] - t[0]))
+sinc_pulse = np.sinc(t)
+rcos_pulse = np.sinc(t/sps) * np.cos(np.pi*B*t/sps) / (1 - (2*B*t/sps)**2)
+
+sinc_spec = np.abs(np.fft.fft(sinc_pulse))
+rc_spec   = np.abs(np.fft.fft(rcos_pulse))
+
+plt.plot(np.fft.fftshift(f), np.fft.fftshift(sinc_spec), label='Sinc')
+plt.plot(np.fft.fftshift(f), np.fft.fftshift(rc_spec),   label=f'Raised Cosine B={B}')
+plt.xlim(-2, 2)
+plt.legend()
+plt.xlabel('Frequency (normalized)')
+plt.title('Bandwidth comparison')
+plt.show()
