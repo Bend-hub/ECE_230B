9058bf3c8491e2f6c77c85024e600424ac7613c3
Benjamin Doo <bendoo529@gmail.com>
2026-05-04 21:12:29 -0700

    commented out gray coding implentation and converted pulse train function back to string of 0s. rc pulse functio implemented

--- ece230b.py
+++ ece230b.py
@@ -1,5 +1,6 @@
 import random
 import math
+import numpy as np
 
 def gen_rand_qam_symbols(N, M):
     '''
@@ -12,24 +13,32 @@ def gen_rand_qam_symbols(N, M):
     constellation: The full M-QAM constellation.
     '''
 
-    bits = math.sqrt(M)
-    symbols = []
-    constellation = []
+    # this works but there's no need for the gray coding
+    '''
     for i in range(N):
         s = random.randint(0, M - 1)
         if M == 16:
             symbols.append('{0:04b}'.format(s))
         elif M == 4:
             symbols.append('{0:02b}'.format(s))
+    '''
         
+    bits = int(math.sqrt(M))
+    symbols = []
+    constellation = []
+    
     # generate constellation
     for a in range(bits):
+        a = (2*a) + 1 - bits
         for b in range(bits):
-            a = (2*a) + 1 - bits
             b = (2*b) + 1 - bits
             # normalize with unit average energy
             A = math.sqrt(3/(2*(M-1)))
             constellation.append(complex(A*a, A*b))
+    
+    for i in range(N):
+        s = random.randint(0, M - 1)
+        symbols.append(constellation[s])
 
     return (symbols, constellation)
 
@@ -42,7 +51,7 @@ def create_pulse_train(symbols, sps):
     Output:
     pulse_train: A discrete-time signal where each symbol is separated by (sps-1) zeros.
     '''
-    zero_arr = [str(0)]*(len(symbols) * (sps - 1)) # one less than sps because we have to insert nonzero numbers
+    zero_arr = [0]*(len(symbols) * (sps - 1)) # one less than sps because we have to insert nonzero numbers
     indices = [(i * sps) for i in range(len(symbols))]
     counter = 0
     for i in indices:
@@ -60,4 +69,21 @@ def get_rc_pulse(beta, span, sps):
     Output:
     pulse: A truncated raised cosine pulse, symmetric and centered at 𝜏=0. The number of zero crossings should be equal to span. The pulse should be normalized such that its energy is equal to one.
     '''
-    pass
+    n = np.arange(-span * sps, span * sps + 1) # arange is end exlusive
+    t = n / sps
+
+    with np.errstate(divide='ignore', invalid='ignore'):
+        numerator   = np.sinc(t) * np.cos(beta * np.pi * t)
+        denominator = 1 - (2 * beta * t) ** 2
+        h = numerator / denominator
+    
+    if beta > 0:
+        t_special = 1 / (2 * beta)
+        special_mask = np.isclose(np.abs(t), t_special)
+        h[special_mask] = (np.pi / 4) * np.sinc(1 / (2 * beta))
+    
+    # Normalize to unit energy
+    energy = np.sum(h**2) / sps
+    h = h / np.sqrt(energy)
+
+    return h
